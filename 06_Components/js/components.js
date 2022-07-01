b5ed304256f0626a1e56b44ebf0acc00f04c7f33
timzo31 <madty31@gmail.com>
2022-07-02 07:42:05 +0800

    Accordion modal.

---
+++ 06_Components/js/components.js
@@ -0,0 +1,8 @@
+"use strict";
+
+///////// ACCORDION //////////
+const modal = document.querySelector(".modal");
+const accordion = document.querySelector(".accordion");
+const item = document.querySelector(".item");
+const itemActive = document.querySelector("item-active");
+const hiddenBox = document.querySelector(".hidden-box");
