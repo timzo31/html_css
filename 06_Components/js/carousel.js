90b46b6383ac5da3b7a0f4721c65bfbfb5e6d07b
timzo31 <madty31@gmail.com>
2022-07-03 00:05:56 +0800

    Carousel Modaj Js completed.

--- 06_Components/js/carousel.js
+++ 06_Components/js/carousel.js
@@ -1,3 +1,48 @@
 "use strict";
 
 /////////// CAROUSEL ////////////
+
+const carouselModal = document.querySelector(".carousel-modal");
+const controlLeft = document.querySelector(".chevron-left");
+const controlRight = document.querySelector(".chevron-right");
+const dots = document.querySelector(".modal_dots");
+const dot = document.querySelectorAll("modal_dot");
+const dotActive = document.querySelector(".modal_dot--active");
+
+// const btnCloseModalCarousel = document.querySelector(
+//   ".btn-close-modal-carousel"
+// );
+
+////////////MODAL OPEN-CLOSE
+const modalCarousel = document.querySelector(".modal-carousel");
+const overlayCar = document.querySelector(".overlay-car");
+const btnCloseModal = document.querySelector(".btn-close-modal-carousel");
+const btnOpenCarouselModal = document.querySelector(".btn-open-carousel");
+
+const openModalCarousel = function () {
+  overlayCar.classList.remove("hidden-car");
+  modalCarousel.classList.remove("hidden-car");
+};
+
+const closeModalCarousel = function () {
+  overlayCar.classList.add("hidden-car");
+  modalCarousel.classList.add("hidden-car");
+};
+
+document.addEventListener("click", function (e) {
+  if (e.key === "Escape") {
+    closeModalCarousel();
+  }
+});
+
+overlayCar.addEventListener("click", closeModalCarousel);
+btnCloseModal.addEventListener("click", closeModalCarousel);
+btnOpenCarouselModal.addEventListener("click", function (e) {
+  e.preventDefault();
+  openModalCarousel();
+});
+
+
+
+
+
