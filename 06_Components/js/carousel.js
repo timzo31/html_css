6984c3d58cd4282cdc5235328eb8e7a9d28f4ac1
timzo31 <madty31@gmail.com>
2022-07-05 23:49:27 +0800

    Pagination Challenge completed successfully.

--- 06_Components/js/carousel.js
+++ 06_Components/js/carousel.js
@@ -2,70 +2,42 @@
 
 /////////// CAROUSEL ////////////
 
-const carouselModal = document.querySelector(".carousel-modal");
-const dots = document.querySelector(".modal_dots");
-const dot = document.querySelectorAll("modal_dot");
-const dotActive = document.querySelector(".modal_dot--active");
-
-// const btnCloseModalCarousel = document.querySelector(
-//   ".btn-close-modal-carousel"
-// );
-
-////////////MODAL OPEN-CLOSE
+const btnOpenCarousel = document.querySelector(".btn-open-carousel");
 const modalCarousel = document.querySelector(".modal-carousel");
 const overlayCar = document.querySelector(".overlay-car");
-const btnCloseModal = document.querySelector(".btn-close-modal-carousel");
-const btnOpenCarouselModal = document.querySelector(".btn-open-carousel");
+const btnCloseCarousel = document.querySelector(".btn-close-modal-carousel");
 
-const openModalCarousel = function () {
-  overlayCar.classList.remove("hidden-car");
-  modalCarousel.classList.remove("hidden-car");
-};
-
-const closeModalCarousel = function () {
-  overlayCar.classList.add("hidden-car");
+const closeCarouselModal = function () {
   modalCarousel.classList.add("hidden-car");
+  overlayCar.classList.add("hidden-car");
 };
 
-document.addEventListener("click", function (e) {
-  if (e.key === "Escape") {
-    closeModalCarousel();
-  }
-});
+const openCarouselModal = function () {
+  modalCarousel.classList.remove("hidden-car");
+  overlayCar.classList.remove("hidden-car");
+};
 
-overlayCar.addEventListener("click", closeModalCarousel);
-btnCloseModal.addEventListener("click", closeModalCarousel);
-btnOpenCarouselModal.addEventListener("click", function (e) {
-  e.preventDefault();
-  openModalCarousel();
+btnOpenCarousel.addEventListener("click", openCarouselModal);
+
+overlayCar.addEventListener("click", closeCarouselModal);
+btnCloseCarousel.addEventListener("click", closeCarouselModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") closeCarouselModal();
+  // e.key === "Escape" && closeCarouselModal;
 });
 
 ///////////// SLIDERS CONTROL /////////////////
+const btnLeft = document.querySelector(".btn-slider-left");
+const btnRight = document.querySelector(".btn-slider-right");
+
 const slides = document.querySelectorAll(".slide");
-const slider = document.querySelector("sliders");
-
-const btnLeft = document.querySelector(".chevron-left");
-const btnRight = document.querySelector(".chevron-right");
-
-let curSlide = 0;
-maxSlide = slides.length;
-
-slider.style.transform = "scale(.5) translateX(-800px)";
+const slider = document.querySelectorAll(".slider");
 slider.style.overflow = "visible";
+slider.style.transform = "scale(.2) translateX(-1600px)";
 
 slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
 
-//Next Slide
-btnRight.addEventListener("click", function () {
-  if (curSlide === maxSlide) {
-    curSlide = 0;
-  } else {
-    curSlide++;
-  }
-  slides.forEach(
-    (s, i) => (s.style.transform = `translateX(${100 * (i - curSlide)}%)`)
-  );
-});
 
 
 
