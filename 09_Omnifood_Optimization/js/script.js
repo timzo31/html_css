7ca387eff9f11089a7eed495d8e32859bd62c907
timzo31 <madty31@gmail.com>
2022-07-11 01:03:54 +0800

    Omnifood Optimizations: Sticky navigation completed successfully.

--- 09_Omnifood_Optimization/js/script.js
+++ 09_Omnifood_Optimization/js/script.js
@@ -58,6 +58,34 @@ allLinks.forEach((link) =>
   })
 );
 
-
+///////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////
+// STICKY NAVIGATION
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    // console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+
+obs.observe(sectionHeroEl);
+
+//////////////////////////////////////////////////
