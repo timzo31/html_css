29bbd6b4fa7c521927423ba2f15a0cba40a1f24d
timzo31 <madty31@gmail.com>
2022-07-16 02:01:33 +0800

    06 Components: Button component modal, JS done.

--- 06_Components/js/components.js
+++ 06_Components/js/components.js
@@ -24,6 +24,26 @@ btnOpenHero.addEventListener("click", openModalHero);
 
 /////////////////////////////////////////////////////
 ///////////////////   BUTTONS   /////////////////////
+const buttonsModal = document.querySelector(".modal_buttons");
+const overlayBut = document.querySelector(".overlay-but");
+const btnCloseModalButtons = document.querySelector(".btn_close_modal-buttons");
+const btnOpenModalButtons = document.querySelector(".btn-open-modal-buttons");
+
+const openModalButton = function (e) {
+  e.preventDefault();
+  buttonsModal.classList.remove("hidden-but");
+  overlayBut.classList.remove("hidden-but");
+};
+
+const closeButtonModal = function () {
+  buttonsModal.classList.add("hidden-but");
+  overlayBut.classList.add("hiddenbut");
+};
+
+btnOpenModalButtons.addEventListener("click", openModalButton);
+
+overlayBut.addEventListener("click", closeButtonModal);
+btnCloseModalButtons.addEventListener("click", closeButtonModal);
 
 
 /////////////////////////////////////////////////////
